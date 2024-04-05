ffbd31c08b7b4d48129b61e0de7b52de6dc2e19b
alexbarry <alexbarry@users.noreply.github.com>
2024-04-05 22:07:21 +0000

    deploy: 264516c35380b51527c067ab7f83663f356d80ff

--- docs/alexgames_wasm.js
+++ docs/alexgames_wasm.js
@@ -38,7 +38,7 @@ Module['ready'] = new Promise((resolve, reject) => {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpxhus_1xc.js
+// include: /tmp/tmpop_m4asc.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -254,21 +254,21 @@ Module['FS_createPath']("/preload/libs", "ui", true, true);
 
   })();
 
-// end include: /tmp/tmpxhus_1xc.js
-// include: /tmp/tmppqchhfma.js
+// end include: /tmp/tmpop_m4asc.js
+// include: /tmp/tmpvnbqzqod.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['ENVIRONMENT_IS_PTHREAD'] || Module['$ww']) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmppqchhfma.js
-// include: /tmp/tmpl34xqrcy.js
+  // end include: /tmp/tmpvnbqzqod.js
+// include: /tmp/tmp5u9ctqrv.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach(function(task) {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpl34xqrcy.js
+  // end include: /tmp/tmp5u9ctqrv.js
 
 
 // Sometimes an existing Module object exists with properties
