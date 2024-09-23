bb6aa444cd379931b2989c9995f14e340603a0b1
Alex Barry <alexbarry@users.noreply.github.com>
2024-09-22 23:22:31 -0300

    i18n: adding Italian translations for web version and solitaire

--- src/lua_scripts/games/solitaire/solitaire_strings.lua
+++ src/lua_scripts/games/solitaire/solitaire_strings.lua
@@ -47,6 +47,29 @@ local translations = {
 		game_option_show_elapsed_time_and_move_count = "Afficher le temps écoulé et le nombre de mouvements",
     },
 
+    it = { 
+        new_game = "Nuova Partita",
+        draw_dropdown_label = "Pesca",
+        draw_type_one = "Uno",
+        draw_type_three = "Tre",
+        start_game = "Inizia il gioco",
+        cancel = "Annulla",
+        
+		move_fmt_str = 'Mosse: %d',
+
+        undo = "Annulla",
+        new_game = "Nuova Partita",
+        autocomplete = "Auto-Completa",
+        
+        loaded_state = "Stato precedente caricato",
+        loaded_state_str = "Impossibile caricare lo stato precedente, non trovato",
+        started_new_game_w_seed = "Generata nuova partita con seed %x %x",
+        loaded_saved_state_bytes = "Caricamento stato salvato: %d byte",
+        no_saved_state_found = "Nessuno stato salvato trovato, avvio di una nuova partita",
+
+		game_option_show_elapsed_time_and_move_count = "Mostra il tempo trascorso e il conteggio delle mosse",
+    }, 
+
     ["zh-hans"] = { 
         new_game = "新游戏",
         draw_dropdown_label = "抽牌",
