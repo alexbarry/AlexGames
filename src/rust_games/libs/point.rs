40fb21789aec5468914bece9823e96578c360147
Alex Barry <alexbarry@users.noreply.github.com>
2024-11-17 16:08:42 -0400

    table_tennis: tweak ball bouncing when hit by paddle

--- src/rust_games/libs/point.rs
+++ src/rust_games/libs/point.rs
@@ -1,3 +1,4 @@
+use std::ops::{Add, Mul};
 
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
 pub struct Pt_g<T> {
@@ -5,11 +6,83 @@ pub struct Pt_g<T> {
     pub y: T,
 }
 
+// TODO clean all of this up, I'm not using it right now
+// but I'll probably want something similar later
+trait Atan2 {
+	fn atan2(self, other: Self) -> Self;
+}
+
+impl Atan2 for f64 {
+	fn atan2(self, other: Self) -> Self {
+		f64::atan2(self, other)
+	}
+}
+
+impl Atan2 for i32 {
+	fn atan2(self, other: Self) -> Self {
+		i32::atan2(self, other)
+	}
+}
+
+trait MagnitudeOps: Add<Output=Self> + Mul<Output=Self> + Sized {
+	fn from_i32(val: i32) -> Self;
+	fn from_f64(val: f64) -> Self;
+	fn sqrt(self) -> Self;
+	fn sin(val: f64) -> Self;
+	fn cos(val: f64) -> Self;
+}
+
+impl MagnitudeOps for f64 {
+	fn from_i32(val: i32) -> Self {
+		val as f64
+	}
+	fn from_f64(val: f64) -> Self {
+		val
+	}
+	fn sqrt(self) -> Self {
+		self.sqrt()
+	}
+	fn sin(val: f64) -> Self {
+		val.sin()
+	}
+	fn cos(val: f64) -> Self {
+		val.cos()
+	}
+}
+
+impl MagnitudeOps for i32 {
+	fn from_i32(val: i32) -> Self {
+		val
+	}
+	fn from_f64(val: f64) -> Self {
+		val as i32
+	}
+	fn sqrt(self) -> Self {
+		(self as f64).sqrt() as i32
+	}
+	fn sin(val: f64) -> Self {
+		(val.sin().round() as i32)
+	}
+	fn cos(val: f64) -> Self {
+		(val.cos().round() as i32)
+	}
+}
+
+
+
 impl<T> Pt_g<T>
 	where T: std::ops::Add<Output = T> +
 	         std::ops::Mul<Output = T> +
-	         Copy,
+	         Copy + Atan2 + MagnitudeOps, f64: From<T>
 {
+
+	pub fn from_mag_angle(mag: T, angle: T) -> Pt_g<T> {
+		Pt_g {
+			y: mag * T::sin(T::from_f64(angle.into()).into()),
+			x: mag * T::cos(T::from_f64(angle.into()).into()),
+		}
+	}
+
     pub fn add(&self, arg: Pt_g<T>) -> Pt_g<T> {
         Pt_g {
             y: self.y + arg.y,
@@ -30,6 +103,14 @@ impl<T> Pt_g<T>
             x: self.y,
         }
     }
+
+	pub fn angle(&self) -> T {
+		self.y.atan2(self.x)
+	}
+
+	pub fn mag(&self) -> T {
+		(self.x*self.x + self.y*self.y).sqrt()
+	}
 }
 
 pub type Pt = Pt_g<i32>;
--- src/rust_games/table_tennis/table_tennis_core.rs
+++ src/rust_games/table_tennis/table_tennis_core.rs
@@ -32,11 +32,14 @@ pub struct State {
 	pub ball: Ptf,
 	ball_velocity: Ptf,
 
+	ball_speed_growth_fact: f64,
+
 	pub game_board_size: Ptf,
+	pub dir_to_start_ball: i32,
 }
 
-const init_ball_vel_y: f64 = 25.0;
-const init_ball_vel_x: f64 = 25.0;
+const init_ball_vel_y: f64 = 35.0;
+const init_ball_vel_x: f64 = 10.0;
 
 impl State {
 	pub fn new(board_size: Pt) -> State {
@@ -54,9 +57,12 @@ impl State {
 
 			player_thickness: 2,
 
+			ball_speed_growth_fact: 4.0,
+			dir_to_start_ball: 1,
+
 			// in game units per second
-			player1_speed: 40,
-			player2_speed: 40,
+			player1_speed: 80,
+			player2_speed: 80,
 		}
 	}
 
@@ -102,13 +108,14 @@ impl State {
 	fn reset_ball_pos(&mut self) {
 		// TODO toggle which direction it goes in
 		self.ball_velocity = Ptf {
-			y: init_ball_vel_y,
+			y: init_ball_vel_y * self.dir_to_start_ball as f64,
 			x: init_ball_vel_x,
 		};
 		self.ball = Ptf {
 			y: self.game_board_size.y/2.0,
 			x: self.game_board_size.x/2.0,
 		};
+		self.dir_to_start_ball *= -1;
 	}
 
 	fn within_player(&self, player: &Player, pos_x: f64) -> bool {
@@ -116,6 +123,16 @@ impl State {
 		let r = self.get_player_pos(player, &Side::BOTTOM_RIGHT).x as f64;
 
 		return l <= pos_x && pos_x <= r;
+	}
+
+	fn mirror_ball_vec(&mut self, dir: f64, dist_to_centre_portion: f64) {
+		let dist_to_centre_portion = dist_to_centre_portion.abs();
+		//let dist_to_centre_portion = 1.0 - dist_to_centre_portion;
+		self.ball_velocity.y *= -1.0;
+		let old_speed = self.ball_velocity.x;
+		let change = self.ball_speed_growth_fact * dist_to_centre_portion;
+		self.ball_velocity.x *= change;
+		println!("prev speed was {:e}, change is {}, new speed is {:e}", old_speed, change, self.ball_velocity.x);
 	}
 
 	pub fn update(&mut self, dt_ms: i32) {
@@ -127,7 +144,9 @@ impl State {
 		if self.ball.y < top_bound {
 			if self.within_player(&Player::PLAYER2, self.ball.x) {
 				println!("ping");
-				self.ball_velocity.y *= -1.0;
+				//self.ball_velocity.y *= -1.0;
+				let dist_to_centre_position = (self.ball.x - self.player2_pos)/self.player2_size as f64;
+				self.mirror_ball_vec(1.0, dist_to_centre_position);
 				self.ball.y = top_bound;
 			} else {
 				println!("point for player1");
@@ -137,10 +156,12 @@ impl State {
 		} else if self.ball.y > bottom_bound {
 			if self.within_player(&Player::PLAYER1, self.ball.x) {
 				println!("pong");
-				self.ball_velocity.y *= -1.0;
+				//self.ball_velocity.y *= -1.0;
+				let dist_to_centre_position = (self.ball.x - self.player1_pos)/self.player1_size as f64;
+				self.mirror_ball_vec(-1.0, dist_to_centre_position);
 				self.ball.y = bottom_bound;
 			} else {
-				println!("point for player1");
+				println!("point for player2");
 				// TODO point for a player
 				self.reset_ball_pos()
 			}
