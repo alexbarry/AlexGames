alexbarry/AlexGames

commit 6a2f18fd288c6d2ac250054c3cedeb5719d47012

core: fix compile error in some common code used by wxWidgets and Android implementations

src/dictionary/c_dictionary.c
@@ -140,7 +140,7 @@ static bool get_next_word_from_frame(const struct word_dict_frame *frame, int *p
 		(*pos)++;
 	}
 	(*pos)++;
-	word_info_out->freq = frame->data + *pos;
+	word_info_out->freq = (word_freq_t*) frame->data + *pos;
 	//memcpy(freq, frame->data + *pos, sizeof(word_freq_t));
 	*pos += sizeof(word_freq_t);
 
@@ -178,6 +178,19 @@ int find_char(const char *str, size_t str_len, char c, int start_pos) {
 		}
 	}
 	return -1;
+}
+
+float str_and_len_to_float(const char *str, size_t len) {
+	#define MAX_LEN (128)
+	char tmp_str[MAX_LEN];
+	if (len-1 >= MAX_LEN) {
+		alex_log_err("[dict] ERROR: str %.*s (len %d) is larger than max buff len %d\n", len, str, len, MAX_LEN);
+		return 0;
+	}
+	memcpy(tmp_str, str, len);
+	tmp_str[len] = '\0';
+	return strtof(tmp_str, NULL);
+	
 }
 
 void *build_word_dict_from_file(const char *fname) {
@@ -259,7 +272,8 @@ void *build_word_dict_from_file(const char *fname) {
 #ifndef ALEXGAMES_WASM
 		// This line is really slow on WASM.
 		// Calling it 200k times seems to add ~500 ms on Firefox 119.0 for linux
-		word_freq_t freq = strtof(freq_str, freq_str + freq_str_len);
+		//word_freq_t freq = strtof(freq_str, freq_str + freq_str_len);
+		word_freq_t freq = str_and_len_to_float(freq_str, freq_str_len);
 #else
 		// TODO refactor this into a nice portable helper function
 		// don't put emscripten specific stuff here.

src/server/socket/socket_server_impl.c
@@ -157,7 +157,8 @@ void get_client_name(void *handle_arg, const char *name_out, size_t max_name_len
 
 	struct sockaddr_storage addr;
 	int len = sizeof(addr);
-	getpeername(handle->socket, &addr, &len);
+	//getpeername(handle->socket, &addr, &len);
+	getpeername(handle->socket, (struct sockaddr*)&addr, &len);
 
 	char addr_str[256];
 	addr_str[0] = 0;

commit 695ba8e80a74676ad8c6c2b08a1c26e9414fb493

dictionary: fix pointer error in dictionary frequency lookup
Introduced yesterday in 6a2f18f.

src/dictionary/c_dictionary.c
@@ -140,7 +140,8 @@ static bool get_next_word_from_frame(const struct word_dict_frame *frame, int *p
 		(*pos)++;
 	}
 	(*pos)++;
-	word_info_out->freq = (word_freq_t*) frame->data + *pos;
+	word_info_out->freq = (word_freq_t*) (frame->data + *pos);
+	//printf("found word %s, freq=%.1e\n", word_info_out->word, *word_info_out->freq);
 	//memcpy(freq, frame->data + *pos, sizeof(word_freq_t));
 	*pos += sizeof(word_freq_t);
 