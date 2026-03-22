64b2c16277404ea4c8d1763319e3e5e50f3cec18
alexbarry <alexbarry@users.noreply.github.com>
2026-03-22 15:27:37 +0000

    deploy: 3b32068414cb8129f30373d8560393082d91f0cf

--- docs/dev/alexgames_wasm.js
+++ docs/dev/alexgames_wasm.js
@@ -84,7 +84,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp_f499gmh.js
+// include: /tmp/tmph23fyn8l.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -254,21 +254,21 @@ Module['FS_createPath']("/preload/libs", "ui", true, true);
 
   })();
 
-// end include: /tmp/tmp_f499gmh.js
-// include: /tmp/tmpxdish6x6.js
+// end include: /tmp/tmph23fyn8l.js
+// include: /tmp/tmpbiqqtu_m.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpxdish6x6.js
-// include: /tmp/tmp1ntzmc1c.js
+  // end include: /tmp/tmpbiqqtu_m.js
+// include: /tmp/tmp0e7d1zr4.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp1ntzmc1c.js
+  // end include: /tmp/tmp0e7d1zr4.js
 
 
 var arguments_ = [];
